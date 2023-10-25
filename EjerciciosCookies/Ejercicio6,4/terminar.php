1ee0ef77a19472ab9c38fb765cd6845e496be75b
Tarkus99 <gabengneb@alu.edu.gva.es>
2023-10-25 08:33:33 +0200

    añado el begin transaxtion

--- EjerciciosCookies/Ejercicio6,4/terminar.php
+++ EjerciciosCookies/Ejercicio6,4/terminar.php
@@ -7,12 +7,15 @@ spl_autoload_register(function ($clase) {
 });
 try {
     $bd = new Base();
+    $bd->link->beginTransaction();
     $pedido = new Pedido($_SESSION['idPedido'], $_SESSION['fecha'], $_SESSION['cliente']);
     $pedido->insertar($bd->link);
     Linea::insertarTodas($bd->link);
+    $bd->link->commit();
     include 'VISTAS/detalleFinal.php';
     session_destroy();
 } catch (Exception $e) {
+    $bd->link->rollBack();
     $mensaje = $e->getMessage();
     include 'VISTAS/mensaje.php';
 }
